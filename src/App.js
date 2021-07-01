4526c22eae42b4a33a02c028a174e4f037f0f90f
Md Alamin <alamiin.bd@gmail.com>
2021-07-02 00:12:50 +0600

    book remove and insert input dynamically

--- src/App.js
+++ src/App.js
@@ -1,20 +1,111 @@
 import './App.css';
-import React  from 'react';
+import React, { Component }  from 'react';
 import Person from './components/Person';
+import {Customer} from './components/Person';
 
-let App = ()=>{
-  return(
-    <div className="App">
-      <h1>
-        Hello it's a text from header function
-      </h1>
-      
-      <Person name="Mohammad Alamin" age= "23"/>
-      <Person name= "Mohammad Arif" age="25"/>
-      <Person name="Rahaim Talukdar" age="29"/>
-    </div>
-  );
+// // functional component 
+//ctrl+k+c for comment and crtl+k=u for uncomment  
+
+// let App = ()=>{
+//   //let person_name = prompt("Enter your name");
+//   //let person_age = parseInt( prompt("Enter your age "));
+//   return(
+//     <div className="App">
+//       <h1>
+//         Hello it's a text from header function
+//       </h1>
+
+//    <Person name=""/*{person_name}*/ age="" /*{person_age}*//>
+//       <Person name= "Mohammad Arif" age="25"/>
+//       <Person name="Rahim Talukdar" age="29"/>
+
+//       <Customer cname= "Sohel Rana" cage="75"/>
+//       <Customer cname= "Sifat Talukdar" cage="57"/>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  state = {
+    books: [
+      { id: 1,  bookName:"The Christmas Pig" , author: "JK Rowling"},
+      { id: 2, bookName:"Maveric Messiah" , author: "Ramesh Kandula"},
+      { id: 3, bookName:"ASOCA: A Sutra" , author: "Irwin Allan Sealy’s"},
+      { id: 4, bookName:"Kam Sutra" , author: "Indian Baincod"}
+    ]
+  }
+  // constructor(){
+  //   super();
+  // }
+
+  //change information with input
+  inputChangeBook = (event, index) =>{
+    //copy books object  into new_book_obj and inside as a object  
+    const new_single_book = {
+      ...this.state.books[index]
+    }
+    new_single_book.bookName = event.target.value;
+    //copy books array  into new_book array
+    const new_books =[...this.state.books];
+    new_books[index]= new_single_book
+
+    this.setState({
+       books : new_books
+    });
+
+  }
+  //change information with input
+  inputChangeAuthor= (event, index)=>{
+    const new_author = {
+      ...this.state.books[index]
+    }
+    new_author.author =event.target.value;
+    const new_books = [...this.state.books];
+    new_books[index]= new_author;
+    this.setState({
+      books:new_books
+    })
+  }
+  
+
+
+  //remove book with click
+  removeBookInput = index =>{
+    //const new_book_obj = this.state.books.slice();
+    //copy books array  into new_book_obj   
+    const new_book_obj =[...this.state.books];
+    new_book_obj.splice(index,1);
+    this.setState({
+      books : new_book_obj
+    });
+  };
+
+  render(){
+
+    const books_obj = this.state.books;
+    const books_map = books_obj.map( (books,index) => {
+      //console.log(books.bookName)
+      return(
+        <Person 
+          bookName= {books.bookName}
+          author = {books.author}
+          remove = { ()=>this.removeBookInput(index) }
+          key={books.id}
+          bookChange = { (event) => this.inputChangeBook(event,index)}
+          authorChange = {(event) => this.inputChangeAuthor(event, index)}
+        />
+      )
+
+    });
+    return(
+      <div className="App">
+        <h2>
+          List of Famous Books and their Authors
+        </h2>
+        {books_map}   
+      </div>
+    )
+  }
 }
-
 
 export default App;
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
 let Person = (props) => {
+  //console.log(props);
     return(
-      <div>
+      <div className="book">
         <p>
-          Name : {props.name} And Age is {props.age} Years Old 
+          Book Name : {props.bookName}
+          <input type="text" className="input" onChange={props.bookChange} value={props.bookName}/>  
         </p>
+        <span>
+          Author :{props.author}
+        </span>
+        <input type="text" className="input" onChange={props.authorChange} value={props.author}/>
+        <div>
+          <button className="books-btn" onClick={props.remove}>Remove this book</button>
+        </div>  
+       
       </div>
     );
 }
 export default Person;
+
+/*
+export class Customer extends Component{
+  constructor(props){
+    super(props)
+    console.log(props);
+    this.cname = props.cname;
+    this.cage = props.cage;
+  }
+  render(){
+    return(
+      <div>
+        <p>
+          Customer name : {this.cname} and age is {this.cage};
+        </p>
+      </div>
+    )
+  }
+}
+*/
